d0ed0b10f7ad017a3a32da9eb97391fcd8b723ce
PAOLA_IBARRA_ORDAZ <a.20308051280514@cbtis128.edu.mx>
2023-04-20 17:44:58 +0000

    vCorrecta

--- lib/main.dart
+++ lib/main.dart
@@ -15,28 +15,101 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Dulcería Alegrías'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Dulcería Alegrías'),
+          backgroundColor: const Color(0xffb80303),
+          actions: const [Icon(Icons.search), Icon(Icons.more_vert)],
+          elevation: 8,
         ),
-      ),
-    );
+        body: Container(
+            padding: EdgeInsets.all(20),
+            child: ListView(
+              children: const [
+                Text(
+                  "Ingresa los siguientes datos",
+                  style: TextStyle(color: Colors.black, fontSize: 22),
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                      hintText: "Ingrese su ID",
+                      labelText: "ID",
+                      prefixIcon: Icon(
+                        Icons.verified_user_outlined,
+                        color: Colors.deepOrange,
+                      )),
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                      hintText: "Ingrese su nombre",
+                      labelText: "Nombre",
+                      prefixIcon: Icon(
+                        Icons.supervised_user_circle,
+                        color: Colors.deepOrange,
+                      )),
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                      hintText: "Ingrese su email",
+                      labelText: "Email",
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.deepOrange,
+                      )),
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                      hintText: "Ingrese su telefono",
+                      labelText: "Telefono",
+                      prefixIcon: Icon(
+                        Icons.call,
+                        color: Colors.deepOrange,
+                      )),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Ingrese su direccion",
+                      labelText: "Direccion",
+                      prefixIcon: Icon(
+                        Icons.add_location_rounded,
+                        color: Colors.deepOrange,
+                      )),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Ingrese su contraseña",
+                      labelText: "Contraseña",
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.deepOrange,
+                      )),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Confirmar su contraseña",
+                      labelText: "Confirmar",
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.deepOrange,
+                      )),
+                ),
+              ],
+            )));
   }
 }
